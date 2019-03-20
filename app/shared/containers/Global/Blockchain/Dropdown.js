b96e9b16bef53ddbb783b457272c56b0ddbdc13d
dafuga <danielfugere28@gmail.com>
2019-03-20 10:26:40 -0700

    chore: added RexInterface container

--- app/shared/containers/Global/Blockchain/Dropdown.js
+++ app/shared/containers/Global/Blockchain/Dropdown.js
@@ -53,7 +53,7 @@ class GlobalBlockchainDropdown extends PureComponent<Props> {
     const options = blockchains
       .filter(b => (
         (
-          (displayTestNetworks && b.testnet)
+          (true && b.testnet)
           || !b.testnet
         )
         && b.chainId !== blockchain.chainId
